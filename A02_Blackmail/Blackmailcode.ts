8ec7bf714e71ff307f874fb57a2b20ca062e06eb
Aketchi-ux <mba49068@stud.hs-furtwangen.de>
2024-10-24 12:11:06 +0200

    update

--- A02_Blackmail/Blackmailcode.ts
+++ A02_Blackmail/Blackmailcode.ts
@@ -1,3 +1,8 @@
 namespace L02_Blackmail{
-    
+    console.log("start");
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(_event: Event): void {
+        console.log(_event);
+    }
 }
