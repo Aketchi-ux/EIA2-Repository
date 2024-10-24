26c09af055cc0059a83927d2a19b4833e8334819
Aketchi-ux <mba49068@stud.hs-furtwangen.de>
2024-10-24 12:36:09 +0200

    update

--- A02_Blackmail/Blackmailcode.ts
+++ A02_Blackmail/Blackmailcode.ts
@@ -3,6 +3,18 @@ namespace L02_Blackmail{
     window.addEventListener("load", handleLoad);
 
     function handleLoad(_event: Event): void {
+        let mail: HTMLElement = <HTMLElement>document.querySelector("div#mail");
+        mail.addEventListener("click", placeLetter);
+        document.addEventListener("keydown", Character);
         console.log(_event);
     }
+
+    function placeLetter(_event: MouseEvent): void {
+        let X: number = _event.offsetX;
+        let Y: number = _event.offsetY;
+    }
+
+    function Character(_event: KeyboardEvent): void {
+        
+    } 
 }
