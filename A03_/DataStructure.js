b59747773bba7397a75a29551c3c927c70ca0a61
Aketchi-ux <mba49068@stud.hs-furtwangen.de>
2024-11-06 11:00:48 +0100

    Update A04

---
+++ A03_/DataStructure.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=DataStructure.js.map
---
+++ A03_/DataStructure.ts
@@ -0,0 +1,26 @@
+const taskList = [
+    {
+        id: 1,
+        name: "Küche putzen",
+        status: "Pending", // Can be "Pending", "Working on", or "Finished"
+        deadline: "2024-11-10" // Optional: date string for deadline
+    },
+    {
+        id: 2,
+        name: "Staubsaugen",
+        status: "Working on",
+        deadline: "2024-11-12"
+    },
+    {
+        id: 3,
+        name: "Spülmaschine ausräumen",
+        status: "Pending",
+        deadline: "2024-11-13"
+    },
+    {
+        id: 4,
+        name: "Fenster putzen",
+        status: "Finished",
+        deadline: "2024-11-15"
+    }
+];
