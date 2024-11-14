d60b3a2d39252442180be6a80eaf1be09175fa7b
Aketchi-ux <mba49068@stud.hs-furtwangen.de>
2024-11-14 14:00:33 +0100

    Update

--- WG Aufgabenliste/data.ts
+++ WG Aufgabenliste/data.ts
@@ -1,18 +1,17 @@
 namespace A03_Formular {
 
     export interface Column {
-        key: string;
+        Titel: string;
         value: string;
+        Date: string;
+        Time: string;
+        Comment: string;
     }
 
-    export interface Data {
-        [value: string]: Column[];
-    }
-
-    export let data: Data = {
-        Tasks: [
+    export let data: Column = {
+        
             {Title:"Einkaufen", For:"Tim", Date:"20.01.25", Time:"10:00", Comment:"Brot,Eis,Kaffe", Status:"in Progress"},
             {Title:"Putzen", For:"Franz", Date:"22.02.25", Time:"09:35", Comment:"Nur Staub putzen", Status:"Pending"}
-        ]
+        
     }
 }
