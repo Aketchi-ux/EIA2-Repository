ecb77fe6a6c4fbcede849e3c3b718b72dd63687b
Aketchi-ux <mba49068@stud.hs-furtwangen.de>
2025-02-10 10:30:13 +0100

    Update

---
+++ MCDonald/06.Subclasses.js
@@ -0,0 +1,8 @@
+"use strict";
+var Subclasses;
+(function (Subclasses) {
+    class Animal {
+    }
+    Subclasses.Animal = Animal;
+})(Subclasses || (Subclasses = {}));
+//# sourceMappingURL=06.Subclasses.js.map
